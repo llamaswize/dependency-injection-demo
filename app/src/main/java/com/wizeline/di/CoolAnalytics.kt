eae8bd6fa8fb5845b5852dfcba9ac3263ae420e6
Daniel Llamas <daniel.llamas@wizeline.com>
2022-01-25 15:30:23 -0600

    Update print text

--- app/src/main/java/com/wizeline/di/CoolAnalytics.kt
+++ app/src/main/java/com/wizeline/di/CoolAnalytics.kt
@@ -1,5 +1,5 @@
 package com.wizeline.di
 
 class CoolAnalytics {
-    fun registerEvent(eventName: String) = println("Registered event named: $eventName")
+    fun registerEvent(eventName: String) = println("Registered event named \"$eventName\" using CoolAnalytics!")
 }
